1649ca632b8ec776c70c47f206e83cdf3bf76542
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2018-09-19 19:02:23 +0300

    Crated url calculator

--- calculator/calculator/urls.py
+++ calculator/calculator/urls.py
@@ -1,4 +1,4 @@
-"""demo URL Configuration
+"""calculator URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-from myfirstapp import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^views', views.greetings, name="hello")
+    url(r'^calc/', include("operations.urls"))
 ]
